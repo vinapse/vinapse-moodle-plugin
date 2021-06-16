de8fb9d9a503a6f5c193a2a79da7d7da764f6123
Tiziano Lattisi <tiziano@axiastudio.it>
2021-06-16 09:05:38 +0200

    Fix use of local_daddy_request_lti_launch with the correct signature.

--- mod/daddyvideo/lti_launch.php
+++ mod/daddyvideo/lti_launch.php
@@ -6,5 +6,5 @@ require_once(dirname(dirname(dirname(__FILE__))).'/local/daddyvideo/locallib.php
 $uuid = optional_param('uuid', NULL, PARAM_STRINGID);
 $department = optional_param('department', NULL, PARAM_STRIND);
 $year = optional_param('year', NULL, PARAM_STRING);
-$content = local_daddy_request_lti_launch($uuid);
+$content = local_daddy_request_lti_launch($uuid, $department, $year);
 echo $content;
