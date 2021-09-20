f1a42eab9f0a3a75898d23036661f375dd69c5e9
Matteo Contrini <matteo@contrini.it>
2021-09-20 16:38:44 +0200

    Release v1.0.0

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var object $plugin */
 $plugin->component = 'mod_daddyvideo';
-$plugin->release = '0.10.3';
-$plugin->version = 2021091300;
+$plugin->release = '1.0.0';
+$plugin->version = 2021092000;
 $plugin->requires = 2020061500;
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_STABLE;
